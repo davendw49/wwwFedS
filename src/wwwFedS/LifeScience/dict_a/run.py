aa79d5acc558848e2cb031d9f8de01ee2192a3c6
davendw49 <davendw49@gmail.com>
2018-11-05 05:07:54 +0800

    miss python

---
+++ src/wwwFedS/LifeScience/dict_a/run.py
@@ -0,0 +1,12 @@
+f = open('Keyword_is_property_ls.txt', 'r', encoding='utf-8')
+f_out = open('Keyword_is_property_ls_clean.txt', 'w')
+c=[]
+sum=0
+s=0
+for line in f:
+	s+=1
+	if line not in c:
+		c.append(line)
+		print(line, file=f_out, end='')
+		sum+=1
+print(sum,s)
